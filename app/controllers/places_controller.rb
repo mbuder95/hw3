8c529024f3e5871e12dba8d9b112d0a299fbb967
Mark Buder <mark.buder@kellogg.northwestern.edu>
2024-02-17 00:57:46 +0000

    HW 3 setup controller, index, and new

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -8,3 +8,20 @@ class PlacesController < ApplicationController
     @entries= Entry.where({"places_id" => @places["id"]})
   end
 end
+
+def new
+end
+
+def create
+  # start with a new Company
+  @places= Place.new
+
+  # assign user-entered form data to Company's columns
+  @places["name"] = params["name"]
+
+  # save Company row
+  @place.save
+
+  # redirect user
+  redirect_to "/places"
+end

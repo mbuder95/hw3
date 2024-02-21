8ba11d3bb17fc04a9c81e2196061a6e217a9cbef
Mark Buder <mark.buder@kellogg.northwestern.edu>
2024-02-21 02:30:34 +0000

    HW3 - part 1 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "places", :action => "index" })
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 resources "entries"
 resources "places"
   # Defines the root path route ("/")
-get("/", { :controller => "places", :action => "index" })
+
 end
